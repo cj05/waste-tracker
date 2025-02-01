b485dd2ae52237a222452f3cea217f36af73a197
cj05 <cjcoolsuper@gmail.com>
2025-02-01 15:30:39 +0700

    spa fix

--- src/components/MenuHeaderProgress.vue
+++ src/components/MenuHeaderProgress.vue
@@ -11,7 +11,7 @@ import HomeButton from './HomeButton.vue'
   <div class="flex justify-between items-start mx-auto my-0 max-sm:flex-col max-sm:items-center">
     <HomeButton />
     <div class="flex flex-col items-center mb-8 gap-5 w-2xl">
-      <div class="flex flex-col items-center gap-5">
+      <div class="flex flex-col items-center">
         <h1 class="text-7xl font-bold text-center text-black max-md:text-6xl max-sm:text-4xl">
           {{ title }}
         </h1>
