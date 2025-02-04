c659d4833a00530ae5cfa7aacb08ce78905c70ca
cj05 <cjcoolsuper@gmail.com>
2025-02-04 21:04:12 +0700

    improvement

--- src/components/History/HistoryCard.vue
+++ src/components/History/HistoryCard.vue
@@ -1,33 +1,31 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'HistoryCard',
-  props: {
-    date: {
-      type: String,
-      required: true
-    },
+<script setup lang="ts">
+import { computed, defineComponent } from 'vue'
+import moment from 'moment';
+const props = defineProps({
+  date: {
+    type: String,
+    required: true
+  },
+  completion:{
+    type:  Number,
+    required:true
   }
 })
+
+const display = computed(()=>moment().diff(props.date, "days"))
+
 </script>
 
 <template>
   <article class="flex gap-5 justify-between mb-16 max-md:flex-col max-sm:mb-10">
     <h2 class="text-5xl font-medium text-black max-md:mb-5 max-md:text-4xl">
-      {{ date }}
+      {{display>1?display+" days ago":display == 1?"Yesterday":"Today"}}
     </h2>
     <div class="flex flex-1 gap-5 max-md:w-full max-sm:flex-col">
-      <div 
-        class="flex-1 rounded bg-slate-400 h-[145px] max-sm:h-[120px]"
-        role="img"
-        aria-label="History content"
-      ></div>
-      <div 
-        class="shrink-0 rounded bg-slate-400 h-[145px] w-[185px] max-sm:w-full max-sm:h-[120px]"
-        role="img"
-        aria-label="History thumbnail"
-      ></div>
+      <div class="flex-1 rounded bg-slate-400 h-[145px] max-sm:h-[120px]" role="img" aria-label="History content"></div>
+      <div class="shrink-0 rounded bg-slate-400 h-[145px] w-[185px] max-sm:w-full max-sm:h-[120px] flex items-center justify-center text-4xl">
+        {{ props.completion }}
+      </div>
     </div>
   </article>
 </template>
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -2,16 +2,21 @@
 import { defineComponent, ref } from 'vue'
 import HistoryCard from '../components/History/HistoryCard.vue'
 import MenuHeader from '@/components/MenuHeader.vue';
-
+import moment from 'moment';
 interface HistoryItem {
   date: string
+  completion:number
+  driverData:{
+
+  }
 }
 
 
 const historyItems: HistoryItem[] = [
-  { date: 'Yesterday' },
-  { date: '2 days ago' },
-  { date: '3 days ago' }
+  { date: moment().subtract(1, 'days').format('YYYYMMDD')   , driverData: {} ,completion:50},
+  { date: moment().subtract(2, 'days').format('YYYYMMDD')   , driverData: {} ,completion:70},
+  { date: moment().subtract(30, 'days').format('YYYYMMDD')   , driverData: {} ,completion:98}
+  //moment(date, "YYYYMMDD").fromNow()
 ]
 const title = ref('History')
 </script>
@@ -21,7 +26,7 @@ const title = ref('History')
     <MenuHeader :title="title" />
 
     <main class="px-5 py-0 mx-auto my-10 max-w-[1400px] max-sm:mx-auto max-sm:my-5">
-      <HistoryCard v-for="item in historyItems" :key="item.date" :date="item.date" />
+      <HistoryCard v-for="item in historyItems" :key="item.date" :date="item.date" :completion="item.completion"/>
     </main>
   </div>
 </template>
